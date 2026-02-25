bc9fa0c28dedb581b5cbde688f919855fe2d3213
txr2855 <1778947680@qq.com>
2026-02-25 10:50:22 +0800

    给main

--- pytest3/218/1/abut GitHub.py
+++ pytest3/218/1/abut GitHub.py
@@ -7,12 +7,13 @@
 2：git remote add origin https://github.com/txr2855/你的GitHub仓库网址.git    就是复制网址
 3：git add .
 4:git commit -m "第一次上传：初始化项目"     里面是注释
-5:git push -u origin master            
+5:git push -u origin main
 第2次以及以后：
 1：git add .
 2:git commit -m "注释写这"
 3:git push
 应该就是这样了，还有我把ssl关了好像
+现在我改了main了
 
 
 
