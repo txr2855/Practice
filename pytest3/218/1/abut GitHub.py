15181be41f190733da477355ebccd078d2360467
txr2855 <1778947680@qq.com>
2026-02-25 10:03:52 +0800

    加入了如何导入github代码文件

---
+++ pytest3/218/1/abut GitHub.py
@@ -0,0 +1,23 @@
+"""
+此页仅仅记录我上传GitHub的方法与问题
+方法：在上传的文化旁点空白处，点那个git bash
+第一次：
+输入
+1：git init
+2：git remote add origin https://github.com/txr2855/你的GitHub仓库网址.git
+3：git add .
+4:git commit -m "第一次上传：初始化项目"
+5:git push -u origin master
+第2次以及以后：
+1：git add .
+2:git commit -m "注释写这"
+3:git push
+应该就是这样了，还有我把ssl关了好像
+
+
+
+
+
+
+"""
+
